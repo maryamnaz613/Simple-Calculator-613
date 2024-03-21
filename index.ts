eab7e2aa8ff82fe81ce354a4a98312d7afe59f15
maryamnaz613 <164339762+maryamnaz613@users.noreply.github.com>
2024-03-21 16:30:30 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,37 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//Printing a Wellcome Message
+console.log("\n\tWellcome To\'CodeWithMaryam\'- CLI Simple Calculator\n");
+
+// Asking Question From Users through Inquirer
+
+let answers = await inquirer.prompt([
+    {message : "Enter First Number", type:"number", name:"firstNumber"},
+    {message: "Enter SeconD Number", type:"number", name: "secondNumber"},
+{
+    message : "Select one operator to perform operation",
+    type: "list",
+    name :"operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+},
+
+]);
+
+// Conditional statement if-else
+if(answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtraction"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log ("Invalid Input")
+}
